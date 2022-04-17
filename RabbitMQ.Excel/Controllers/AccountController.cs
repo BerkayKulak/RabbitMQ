edc24e767501a348bbcb66a4ca54bb12f45f5203
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-04-17 18:05:33 +0300

    2. Adım :AccountController > Login sayfasının oluşturulması

---
+++ RabbitMQ.Excel/Controllers/AccountController.cs
@@ -0,0 +1,46 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RabbitMQ.Excel.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(string Email, string Password)
+        {
+            var hasUser = await _userManager.FindByEmailAsync(email: Email);
+
+            if (hasUser == null)
+            {
+                return View();
+            }
+
+            var signInResult = await _signInManager.PasswordSignInAsync(hasUser, password: Password, true, false);
+
+            if (!signInResult.Succeeded)
+            {
+                return View();
+            }
+
+            return RedirectToAction("Index", "Home");
+
+            
+        }
+
+    }
+}
---
+++ RabbitMQ.Excel/Controllers/ProductController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RabbitMQ.Excel.Controllers
+{
+    [Authorize]
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
