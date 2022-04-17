972beaae3b780eea1041c04da5d98435a7561303
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-04-17 16:29:54 +0300

    4. Adım : RabbitMQPublisher class’sının oluşturulması

---
+++ RabbitMQ.Watermark/Services/RabbitMQPublisher.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using System.Text.Json;
+
+namespace RabbitMQ.Watermark.Services
+{
+    public class RabbitMQPublisher
+    {
+        private readonly RabbitMQClientService _rabbitMQClientService;
+
+        public RabbitMQPublisher(RabbitMQClientService rabbitMqClientService)
+        {
+            _rabbitMQClientService = rabbitMqClientService;
+        }
+
+        public void Publish(productImageCreatedEvent productImageCreatedEvent)
+        {
+            var channel = _rabbitMQClientService.Connect();
+
+            var bodyString = JsonSerializer.Serialize(productImageCreatedEvent);
+
+            var bodyByte = Encoding.UTF8.GetBytes(bodyString);
+
+            var properties = channel.CreateBasicProperties();
+
+            properties.Persistent = true;
+
+            channel.BasicPublish(exchange: RabbitMQClientService.ExchangeName, routingKey: RabbitMQClientService.RoutingWatermark, basicProperties: properties, body: bodyByte, mandatory: true);
+        }
+    }
+}
