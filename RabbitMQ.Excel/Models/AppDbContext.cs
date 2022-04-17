54e83e565d1a14e1d94a18cf6e80168532aa3520
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-04-17 17:45:37 +0300

    1. Adım :Projenin oluşturulması

---
+++ RabbitMQ.Excel/Models/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace RabbitMQ.Excel.Models
+{
+    public class AppDbContext : IdentityDbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<UserFile> UserFiles { get; set; }
+    }
+}
