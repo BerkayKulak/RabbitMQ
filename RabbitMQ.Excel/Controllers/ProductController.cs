5a273113b1e5168d85998ff3327b35fc37ab6fcc
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-04-17 18:28:58 +0300

    5. Adım :RabbitMQPublisher class’sımızın oluşturulması

--- RabbitMQ.Excel/Controllers/ProductController.cs
+++ RabbitMQ.Excel/Controllers/ProductController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using RabbitMQ.Excel.Models;
+using RabbitMQ.Excel.Services;
 using System;
 using System.Linq;
 using System.Threading.Tasks;
@@ -14,11 +15,13 @@ namespace RabbitMQ.Excel.Controllers
     {
         private readonly AppDbContext _context;
         private readonly UserManager<IdentityUser> _userManager;
+        private readonly RabbitMQPublisher _rabbitMqPublisher;
 
-        public ProductController(AppDbContext context, UserManager<IdentityUser> userManager)
+        public ProductController(AppDbContext context, UserManager<IdentityUser> userManager, RabbitMQPublisher rabbitMqPublisher)
         {
             _context = context;
             _userManager = userManager;
+            _rabbitMqPublisher = rabbitMqPublisher;
         }
 
         public IActionResult Index()
@@ -42,6 +45,8 @@ namespace RabbitMQ.Excel.Controllers
             await _context.UserFiles.AddAsync(userfile);
 
             await _context.SaveChangesAsync();
+
+            _rabbitMqPublisher.Publish(new Shared.CreateExcelMessage() { FileId = userfile.Id, UserId = user.Id });
 
             TempData["StartCreatingExcel"] = true;
 
---
+++ RabbitMQ.Excel/Services/RabbitMQPublisher.cs
@@ -0,0 +1,31 @@
+﻿using Shared;
+using System.Text;
+using System.Text.Json;
+
+namespace RabbitMQ.Excel.Services
+{
+    public class RabbitMQPublisher
+    {
+        private readonly RabbitMQClientService _rabbitMQClientService;
+
+        public RabbitMQPublisher(RabbitMQClientService rabbitMqClientService)
+        {
+            _rabbitMQClientService = rabbitMqClientService;
+        }
+
+        public void Publish(CreateExcelMessage createExcelMessage)
+        {
+            var channel = _rabbitMQClientService.Connect();
+
+            var bodyString = JsonSerializer.Serialize(createExcelMessage);
+
+            var bodyByte = Encoding.UTF8.GetBytes(bodyString);
+
+            var properties = channel.CreateBasicProperties();
+
+            properties.Persistent = true;
+
+            channel.BasicPublish(exchange: RabbitMQClientService.ExchangeName, routingKey: RabbitMQClientService.RoutingExcel, basicProperties: properties, body: bodyByte, mandatory: true);
+        }
+    }
+}
