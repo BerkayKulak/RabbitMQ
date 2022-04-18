ef7ab545d9382020126d5c0c5750b2f0ad2660ae
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-04-18 10:38:39 +0300

    Word dosyasını Pdf dosyasına dönüştürme(Asp.Net Core MVC + RabbitMQ)-1

---
+++ RabbitMQ.WordToPdf.Producer/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RabbitMQ.WordToPdf.Producer.Models;
+using System.Diagnostics;
+
+namespace RabbitMQ.WordToPdf.Producer.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult WordToPdfPage()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult WordToPdfPage(Models.WordToPdf wordToPdf)
+        {
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ RabbitMQ.WordToPdf.Producer/Models/WordToPdf.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace RabbitMQ.WordToPdf.Producer.Models
+{
+    public class WordToPdf
+    {
+        public string Email { get; set; }
+        public IFormFile WordFile { get; set; }
+    }
+}
