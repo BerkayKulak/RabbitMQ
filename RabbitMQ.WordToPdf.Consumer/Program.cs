cce1ba25be74e99f9eca08424a861beede5986a3
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-04-18 11:41:58 +0300

    Word dosyasını Pdf dosyasına dönüştürme(Asp.Net Core MVC + RabbitMQ)-3

---
+++ RabbitMQ.WordToPdf.Consumer/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using System.Net.Mail;
+using System.Net.Mime;
+
+namespace RabbitMQ.WordToPdf.Consumer
+{
+    internal class Program
+    {
+
+        public static bool EmailSend(string email, MemoryStream memoryStream, string fileName)
+        {
+            try
+            {
+                memoryStream.Position = 0;
+
+                System.Net.Mime.ContentType ct = new ContentType(System.Net.Mime.MediaTypeNames.Application.Pdf);
+
+                System.Net.Mail.Attachment attach = new System.Net.Mail.Attachment(memoryStream, ct);
+
+                attach.ContentDisposition.FileName = $"{fileName}.pdf";
+
+                MailMessage mailMessage = new MailMessage();
+
+                SmtpClient smtpClient = new SmtpClient();
+
+                mailMessage.From = new MailAddress("kulakberkay15@gmail.com");
+
+                mailMessage.To.Add(email);
+
+                mailMessage.Subject = "Pdf Dosyası | bernersoft.com";
+
+                mailMessage.Body = "pdf dosyanız ektedir";
+
+                mailMessage.IsBodyHtml = true;
+
+                mailMessage.Attachments.Add(attach);
+
+                smtpClient.Host = "smtp.gmail.com";
+
+                smtpClient.Port = 587;
+
+                smtpClient.EnableSsl = true;
+
+                smtpClient.Credentials = new System.Net.NetworkCredential("kulakberkay15@gmail.com", "43795164825Rt*");
+
+                Console.WriteLine($"Sonuç:{email} adresine gönderilmiştir");
+
+                memoryStream.Close();
+
+                memoryStream.Dispose();
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Mail gönderim sırasında bir hata meydana geldi: {ex.InnerException}");
+                return false;
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
